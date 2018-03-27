b1ca998ea0bc109942c69822e8dde2a0713194d0
ksyunnnn <l.o.s19kk@gmail.com>
2018-03-27 20:36:11 +0900

    メッセージ一覧実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,8 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+// notice消す
+window.setTimeout(()=>{
+  document.querySelector('.notice').classList.add('hidden-with-animation')
+}, "3000")
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
+
+  get '/groups', to: 'groups#index'
 end
